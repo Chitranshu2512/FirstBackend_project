109942dfaa6dcc14ce1d3a5d19685e21e53cb8a9
Ronak Kushwaha <ronakkushwaha123@gmail.com>
2024-07-18 10:03:42 +0530

    file handeling

---
+++ src/utils/cloudinary.js
@@ -0,0 +1,34 @@
+import { v2 as cloudinary } from 'cloudinary'
+import fs from "fs"
+
+    // Configuration
+    cloudinary.config({ 
+        cloud_name: process.env.CLOUDINARY_CLOUD_NAME, 
+        api_key: process.env.CLOUDINARY_API_KEY, 
+        api_secret: process.env.CLOUDINARY_SECRET
+    });
+
+
+    const updloadOnCloudinary = async(localFilePath) => {
+        try {
+
+            if(!localFilePath) return null
+
+            // upload file on cloudinary
+            const response = await cloudinary.uploader.upload(localFilePath, {
+                resource_type: 'auto',
+            })
+            // console the response
+            console.log("File is uploaded on Cloudinary", response.url)
+            return response
+        } 
+
+        catch (error) {
+            fs.unlinkSync(localFilePath)       // it remove the locally saved temp file on our backend server as the upload on cloudinary operation got failed.
+
+            return null
+        }
+    }
+
+
+export {updloadOnCloudinary}
